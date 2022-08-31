62dea0178fb4185ea3a102afe9d3776eb2dce4f3
arrowbot1 <112566553+arrowbot1@users.noreply.github.com>
2022-08-31 20:41:25 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,11 +19,11 @@ global.APIKeys = {
 }
 
 //customize settings\\
-global.owner = ['Your Number']
-global.premium = ['Your Number']
-global.ownernomer = 'Your Number'
-global.ownername = '🐦Star Boy Nexus'
-global.botname = 'Gojo-Satoru'
+global.owner = ['94705577307']
+global.premium = ['94705577307']
+global.ownernomer = '94717874659'
+global.ownername = '🐦Mr. Dark Arrow OFC'
+global.botname = 'Dark Arrow Bot'
 global.footer = '©NexusNwInc.'
 global.ig = 'https://github.com/nexusNw'
 global.region = 'India, South India, Kerala'
